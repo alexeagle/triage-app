200768b01aecc08578d3b6e288280c0813a74059
Alex Eagle <alex@aspect.dev>
2025-11-25 16:43:27 -0800

    add secret

--- lib/authConfig.ts
+++ lib/authConfig.ts
@@ -9,6 +9,7 @@ import GitHubProvider from "next-auth/providers/github";
 import { upsertUser } from "./auth";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET!,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
