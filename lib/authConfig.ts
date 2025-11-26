f5dba879400563023fa8b20e55b742361e8bf06c
Alex Eagle <alex@aspect.dev>
2025-11-25 16:48:02 -0800

    add debug

--- lib/authConfig.ts
+++ lib/authConfig.ts
@@ -9,6 +9,7 @@ import GitHubProvider from "next-auth/providers/github";
 import { upsertUser } from "./auth";
 
 export const authOptions: NextAuthOptions = {
+  debug: true,
   secret: process.env.NEXTAUTH_SECRET!,
   providers: [
     GitHubProvider({
